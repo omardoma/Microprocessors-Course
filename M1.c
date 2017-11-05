f99ee0ac2c7e60d4426851b23b43d595ed8ecad5
omardoma <omar.elias.96@gmail.com>
2017-11-05 09:44:52 +0200

    initial commit

---
+++ M1.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int avgSignal(int signal[], int kernel[])
+{
+    int midIndex = (10 / 2) - 1;
+    int j = 0;
+    for (int i = 0; i < 10; i++)
+    {
+        signal[i] = avgElement(midIndex, &j, signal, kernel);
+        midIndex--;
+    }
+}
+
+int avgElement(int midIndex, int *j, int signal[], int kernel[])
+{
+    int sum = 0;
+    int i;
+    for (i = *j; i < 7 && i < 10-midIndex; i++)
+    {
+        if (midIndex < 0)
+        {
+            (*j)++;
+        }
+        sum += signal[i];
+    }
+
+    return (sum / ((i - *j) + 1));
+}
+
+int main()
+{
+    int signal[7] = {1, 2, 3, 4, 5, 6, 7};
+    int kernel[10] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    avgSignal(signal, kernel);
+    for (int i = 0; i < 7; i++)
+    {
+        printf("%d", signal[i]);
+        printf("%s", " ");
+    }
+    return 0;
+}
