d5ce79beb6e07fa8863c27c9f09bc8a6f06edf5d
omardoma <omar.elias.96@gmail.com>
2017-11-09 17:33:51 +0200

    Merge

--- M1.c
+++ M1.c
@@ -1,5 +1,5 @@
-int kernelLength = 0;
-int signalLength = 0;
+int kernelLength;
+int signalLength;
 
 int avgElement(int midIndex, int *signal)
 {
@@ -23,18 +23,6 @@ int avgElement(int midIndex, int *signal)
     return (sum / (i - intersectionIndex));
 }
 
-int *avgSignal(int *signal, int *kernel)
-{
-    int midIndex = (kernelLength / 2) - 1;
-    int *result = malloc(signalLength * sizeof(int));
-    int i = 0;
-    while (i < signalLength)
-    {
-        result[i] = avgElement(midIndex--, signal);
-        i++;
-    }
-    return result;
-}
 
 main()
 {
@@ -42,26 +30,38 @@ main()
     int kernel[10];
     int signal[7];
     int i = 0;
-    signal[0] = 1;
-    signal[1] = 2;
-    signal[2] = 3;
-    signal[3] = 4;
-    signal[4] = 5;
-    signal[5] = 6;
-    signal[6] = 7;
+    // signal[0] = 1;
+    // signal[1] = 2;
+    // signal[2] = 3;
+    // signal[3] = 4;
+    // signal[4] = 5;
+    // signal[5] = 6;
+    // signal[6] = 7;
 
-    kernel[0] = 1;
-    kernel[1] = 1;
-    kernel[2] = 1;
-    kernel[3] = 1;
-    kernel[4] = 1;
-    kernel[5] = 1;
-    kernel[6] = 1;
-    kernel[7] = 1;
-    kernel[8] = 1;
-    kernel[9] = 1;
+    // kernel[0] = 1;
+    // kernel[1] = 1;
+    // kernel[2] = 1;
+    // kernel[3] = 1;
+    // kernel[4] = 1;
+    // kernel[5] = 1;
+    // kernel[6] = 1;
+    // kernel[7] = 1;
+    // kernel[8] = 1;
+    // kernel[9] = 1;
 
     kernelLength = sizeof(kernel) / sizeof(int);
     signalLength = sizeof(signal) / sizeof(int);
-    result = avgSignal(signal, kernel);
+
+    int midIndex = (kernelLength / 2) - 1;
+    result = malloc(signalLength * sizeof(int));
+    while (i < signalLength)
+    {
+        result[i] = avgElement(midIndex--, signal);
+        i++;
+    }
+
+    for (i = 0; i < signalLength; i++)
+    {
+        printf("%d ", result[i]);
+    }
 }
