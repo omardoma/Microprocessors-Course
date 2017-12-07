9c1cd0ee3490d3fd75a75c1e34a3a15ae6122b54
omardoma <omar.elias.96@gmail.com>
2017-12-07 15:26:36 +0200

    Fix M2

--- M2.c
+++ M2.c
@@ -1,15 +1,15 @@
 #include <xmmintrin.h> //Header for intrinsic functions
 #include <stdio.h>
 #include <time.h>
-
+#define MAX_DIM 2000
+float a[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
+float b[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
+float c[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
+float d[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
 int main()
 {
     // Variables definition and initialization
-    int MAX_DIM = 100;
-    float a[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
-    float b[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
-    float c[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
-    float d[MAX_DIM][MAX_DIM] __attribute__((aligned(16)));
+
     for (int i = 0; i < MAX_DIM; ++i)
     {
         for (int j = 0; j < MAX_DIM; ++j)
@@ -35,26 +35,31 @@ int main()
     clock_t Time2 = clock();
     clock_t Time3 = clock();
     // YOUR CODE HERE
-
-    int i, count = 0;
-    while (i < MAX_DIM)
+    __m128 *lvfA, *lvfB;
+    float *lafResult;
+    int count = 0;
+    for (int i=0; i < MAX_DIM; i++)
     {
         int sum = 0;
-        for (int j = 0; j < MAX_DIM; j += 4)
+        for (int j = 0; j < MAX_DIM; j++)
         {
-            float lafResult[] = {0.0f, 0.0f, 0.0f, 0.0f};
-            __m128 lvfA = _mm_set_ps(a[i][j], a[i][j + 1], a[i][j + 2], a[i][j + 3]);
-            __m128 lvfB = _mm_set_ps(b[j][i], b[j + 1][i], b[j + 2][i], b[j + 3][i]);
-            __m128 lvfA_mul_B = _mm_mul_ps(lvfA, lvfB);
-            _mm_store_ps(lafResult, lvfA_mul_B);
-            sum += lafResult[0] + lafResult[1] + lafResult[2] + lafResult[3];
+            for (int k = 0; k < MAX_DIM; k += 4)
+            {
+                // float lafResult[] = {0.0f, 0.0f, 0.0f, 0.0f};
+                lvfA = (__m128 *)&a[i];
+                lvfB = (__m128 *)&b[j];
+                // lvfB = _mm_set_ps(b[j][i], b[j + 1][i], b[j + 2][i], b[j + 3][i]);
+                __m128 lvfA_mul_B = _mm_mul_ps(*lvfA, *lvfB);
+                // _mm_store_ps(lafResult, lvfA_mul_B);
+                lafResult = (float*)&lvfA_mul_B;
+                d[i][j] += lafResult[0] + lafResult[1] + lafResult[2] + lafResult[3];
+            }
+            lvfA++;
+            lvfB++;
+
+            
         }
-        d[i][count] = sum;
-        if (++count == MAX_DIM)
-        {
-            count = 0;
-            i++;
-        }
+
     }
 
     clock_t Time4 = clock();
@@ -65,7 +70,7 @@ int main()
     printf(" Time taken by SIMD optimized code is %.7f \n", TotalTimeSIMD);
 
     //Print the matrices for comparison
-    
+
     // for (int i = 0; i < MAX_DIM; i++)
     // {
     //     for (int j = 0; j < MAX_DIM; j++)
